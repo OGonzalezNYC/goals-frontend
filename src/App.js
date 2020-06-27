c4e3e5e1ee7f991c82438bb4de1b3e8f206a492d
Omar Gonzalez <OGonzalezNYC@gmail.com>
2020-06-27 03:15:13 -0400

    set up goalReducer, set up folders, set up fetchGoals action, played with synchronous dispatch through action creator, connected App component to redux store

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
+import {connect} from 'react-redux';
+//this gives the App component the ability to access the store; and App implements that ability below, at the very last line of code in this file
+
+import {fetchGoals} from './actions/fetchGoals';
+// this makes the fetchGoals() function in the fetchGoals file a prop of App.  (And the brackets around "fetchGoals" are necessary since that file is not EXPORT defaulted).
+
 class App extends React.Component {
 
   componentDidMount() {
-    // fetch('http://localhost:3000/api/v1/goals/1/steps', {
+console.log("HERE")
+    this.props.fetchGoals({type: 'FETCH_GOALS', payload: {mission: "Win!"}})
+    //this is the action object that fetchGoals dispatches below to the reducer. In this case, the payload is just dummy data.
+
+    // // fetch('http://localhost:3000/api/v1/goals/1/steps', {
+    // // })
+    // fetch('http://localhost:3000/api/v1/goals', {
     //   method: 'GET'
     // })
-    fetch('http://localhost:3000/api/v1/goals', {
-      method: 'GET'
-    })
-    //no need to specify "method: 'GET'", as GET is the default method for a fetch() request.
-    //console.log("HERE") CHECK
-    .then(response => response.json())
-    //console.log("HERE") CHECK
-    .then(jsonizedResponse => console.log(jsonizedResponse))
-    //console.log("HERE") CHECK
+    // //no need to specify "method: 'GET'", as GET is the default method for a fetch() request.
+    // //console.log("HERE") CHECK
+    // .then(response => response.json())
+    // //console.log("HERE") CHECK
+    // .then(jsonizedResponse => console.log(jsonizedResponse))
+    // //console.log("HERE") CHECK
   }
 
   render() {
@@ -28,7 +37,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
+// const mapStateToProps = (state) => {
+//   return {
+//     goals: state.goals
+//   }
+// }
+
+export default connect(null, {fetchGoals})(App);
+//null because we don't currently care about OBTAINING state; and {fetchGoals} is the equivalent of mapDispatchToProps, which sends the action object to the producer
 
 
 
